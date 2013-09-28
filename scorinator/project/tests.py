b421926370b7fa920e73bf49053b3dcc2a89c51f
Greg Reinbach <greg@reinbach.com>
2013-09-28 08:25:21 -0400

    added basic initial tests, test discovery sucks though

--- scorinator/project/tests.py
+++ scorinator/project/tests.py
@@ -1,16 +1,24 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
+from django_dynamic_fixture import G
+
+from project.models import Project
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestProject(TestCase):
+    def test_ordering(self):
+        G(Project, name="abc")
+        G(Project, name="zxy")
+        G(Project, name="mnc")
+
+        assert [p.name for p in Project.objects.all()] == ['abc', 'mnc', 'zxy']
+
+
+class TestProjectListView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get(self):
+        response = self.client.get(reverse("project.list"))
+        assert response.status_code == 200
