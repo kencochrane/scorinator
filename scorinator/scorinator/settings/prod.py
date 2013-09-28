7cb5c6019a8d376edfb4b383b3f8baecf9cf93a0
Ken Cochrane <KenCochrane@gmail.com>
2013-09-27 21:24:30 -0400

    changed the log file name

--- scorinator/scorinator/settings/prod.py
+++ scorinator/scorinator/settings/prod.py
@@ -21,7 +21,7 @@ LOGGING['loggers']['opbeat.errors'] = {
             'handlers': ['console'],
             'propagate': False}
 
-LOGGING['handlers']['log_file']['filename'] = '/var/log/scorinator.log'
+LOGGING['handlers']['log_file']['filename'] = ''
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
