c5f3f67a661f0614634163704d7ba61d67e59abc
Ken Cochrane <KenCochrane@gmail.com>
2013-09-27 21:32:56 -0400

    fix the prod logging issue

--- scorinator/scorinator/settings/prod.py
+++ scorinator/scorinator/settings/prod.py
@@ -9,19 +9,62 @@ ADMINS = (
 
 DATABASES = {'default': dj_database_url.config()}
 
-LOGGING['root'] = {
-            'level': 'WARNING',
-            'handlers': ['opbeat']}
-LOGGING['loggers']['opbeat'] = {
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+     'require_debug_false': {
+         '()': 'django.utils.log.RequireDebugFalse'
+     }},
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+            },
+        'console': {
             'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False}
-LOGGING['loggers']['opbeat.errors'] = {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins'],
             'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False}
-
-LOGGING['handlers']['log_file']['filename'] = ''
+            'propagate': True,
+            },
+        'django.request': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+            },
+        'django.db.backends': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'INFO',
+            'propagate': False,
+            },
+        # Catch All Logger -- Captures any other logging
+        '': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'DEBUG',
+            'propagate': True,
+            }
+    }
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
