83d24b25bc7b69a2e578150a92435b011bc38da8
John Costa <john.costa@gmail.com>
2013-09-28 15:48:17 -0400

    add api-auth endpoint

--- scorinator/scorinator/urls.py
+++ scorinator/scorinator/urls.py
@@ -6,4 +6,9 @@ urlpatterns = patterns(
     '',
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^project/', include('project.urls')),
+
+    # TODO we may want to disable this later post development.
+    #  for now, it's useful to understand what is available
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework'))
 )
