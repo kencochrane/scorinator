dec479aef7a3449b8b9afffea75f47d4616e716f
Greg Reinbach <greg@reinbach.com>
2013-09-28 21:14:57 -0400

    tweaked repo url look on project add template, still to validate data

--- scorinator/project/forms.py
+++ scorinator/project/forms.py
@@ -3,6 +3,8 @@ from project.models import Project
 
 
 class ProjectForm(forms.ModelForm):
+    url = forms.URLField(label="Github URL")
+
     class Meta:
         model = Project
-        exclude = ('date_added', 'last_update', 'slug')
+        exclude = ('date_added', 'last_update', 'slug', 'repo_url')
