9b4cf9ffeb1ce6a6a03b71d39982ee428896ca7c
John Costa <john.costa@gmail.com>
2013-09-29 15:15:25 -0400

    pep8

--- scorinator/project/migrations/0002_remove_scores.py
+++ scorinator/project/migrations/0002_remove_scores.py
@@ -17,47 +17,64 @@ class Migration(SchemaMigration):
         # Deleting model 'ScoreAttribute'
         db.delete_table(u'project_scoreattribute')
 
-
     def backwards(self, orm):
         # Adding model 'ProjectScore'
         db.create_table(u'project_projectscore', (
-            ('last_updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('total_score', self.gf('django.db.models.fields.DecimalField')(max_digits=8, decimal_places=2)),
-            ('project', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['project.Project'])),
-            ('date_added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('last_updated', self.gf('django.db.models.fields.DateTimeField')(
+                auto_now=True, blank=True)),
+            ('total_score', self.gf('django.db.models.fields.DecimalField')(
+                max_digits=8, decimal_places=2)),
+            ('project', self.gf('django.db.models.fields.related.ForeignKey')(
+                to=orm['project.Project'])),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(
+                auto_now_add=True, blank=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(
+                primary_key=True)),
         ))
         db.send_create_signal(u'project', ['ProjectScore'])
 
         # Adding model 'ProjectScoreAttribute'
         db.create_table(u'project_projectscoreattribute', (
-            ('project_score', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['project.ProjectScore'])),
-            ('score_value', self.gf('django.db.models.fields.DecimalField')(max_digits=8, decimal_places=2)),
-            ('score_attribute', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['project.ScoreAttribute'])),
+            ('project_score',
+             self.gf('django.db.models.fields.related.ForeignKey')(
+                 to=orm['project.ProjectScore'])),
+            ('score_value', self.gf('django.db.models.fields.DecimalField')(
+                max_digits=8, decimal_places=2)),
+            ('score_attribute',
+             self.gf('django.db.models.fields.related.ForeignKey')(
+                 to=orm['project.ScoreAttribute'])),
             ('result', self.gf('django.db.models.fields.TextField')()),
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(
+                primary_key=True)),
         ))
         db.send_create_signal(u'project', ['ProjectScoreAttribute'])
 
         # Adding model 'ScoreAttribute'
         db.create_table(u'project_scoreattribute', (
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(
+                max_length=50)),
             ('description', self.gf('django.db.models.fields.TextField')()),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(
+                max_length=50)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(
+                primary_key=True)),
         ))
         db.send_create_signal(u'project', ['ScoreAttribute'])
-
 
     models = {
         u'project.project': {
             'Meta': {'ordering': "('name',)", 'object_name': 'Project'},
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [],
+                           {'auto_now_add': 'True', 'blank': 'True'}),
             'description': ('django.db.models.fields.TextField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
-            'repo_url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+            u'id': ('django.db.models.fields.AutoField', [],
+                    {'primary_key': 'True'}),
+            'last_updated': ('django.db.models.fields.DateTimeField', [],
+                             {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [],
+                     {'max_length': '150'}),
+            'repo_url': ('django.db.models.fields.URLField', [],
+                         {'max_length': '200'})
         }
     }
 
