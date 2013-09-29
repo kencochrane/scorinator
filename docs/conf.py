7151d36c2a87553a14477ea2413d1d8056781fc3
John Costa <john.costa@gmail.com>
2013-09-29 15:24:35 -0400

    pep8

--- docs/conf.py
+++ docs/conf.py
@@ -184,23 +184,22 @@ htmlhelp_basename = 'Scorinatordoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto/manual]).
-latex_documents = [
-  ('index', 'Scorinator.tex', u'Scorinator Documentation',
-   u'Ken Cochrane, Greg Reinbach, John Costa', 'manual'),
-]
+latex_documents = [(
+    'index', 'Scorinator.tex', u'Scorinator Documentation',
+    u'Ken Cochrane, Greg Reinbach, John Costa', 'manual'), ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -241,11 +240,10 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-  ('index', 'Scorinator', u'Scorinator Documentation',
-   u'Ken Cochrane, Greg Reinbach, John Costa', 'Scorinator', 'One line description of project.',
-   'Miscellaneous'),
-]
+texinfo_documents = [(
+    'index', 'Scorinator', u'Scorinator Documentation',
+    u'Ken Cochrane, Greg Reinbach, John Costa', 'Scorinator',
+    'One line description of project.', 'Miscellaneous'), ]
 
 # Documents to append as an appendix to all manuals.
 #texinfo_appendices = []
