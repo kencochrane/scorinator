048b7ab51c033d8fc82ad67bbd645af67d5d5e7f
Ken Cochrane <KenCochrane@gmail.com>
2013-09-29 18:23:05 -0400

    adding new attributes

---
+++ worker/attributes/attrib_github_pull_requests.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+ATTRIBUTE_SLUG = 'repo_pullrequests'
+WEIGHT = 10
+
+
+def run(project):
+    """ Look to see if a project has a readme file """
+    repo_url = project.get('repo_url', None)
+    if not repo_url:
+        return None
+
+    repo_url = repo_url.replace("github.com/", "api.github.com/repos/")
+
+    r = requests.get("{0}/pulls".format(repo_url))
+    if r and r.status_code == 200:
+        value = len(r.json())
+    else:
+        return None
+
+    return {'name': ATTRIBUTE_SLUG, 'value': value}
+
+def score(project):
+    return
---
+++ worker/attributes/attrib_github_releases.py
@@ -0,0 +1,27 @@
+application/vnd.github.manifold-preview
+
+import os
+import requests
+
+ATTRIBUTE_SLUG = 'project_releases'
+WEIGHT = 10
+
+
+def run(project):
+    """ Look to see if a project has a readme file """
+    repo_url = project.get('repo_url', None)
+    if not repo_url:
+        return None
+
+    repo_url = repo_url.replace("github.com/", "api.github.com/repos/")
+    headers = {'Accept': 'application/vnd.github.manifold-preview'}
+    r = requests.get("{0}/releases".format(repo_url), headers=headers)
+    if r and r.status_code == 200:
+        value = len(r.json())
+    else:
+        return None
+
+    return {'name': ATTRIBUTE_SLUG, 'value': value}
+
+def score(project):
+    return
